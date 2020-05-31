cc77c0ba04e5f095a10faeaf79cfbb748b773c66
Jhonatan Lujan <wlujan@xirectss.com>
2020-05-30 20:01:04 -0500

    Variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -7,14 +7,16 @@ process.env.PORT = process.env.PORT || 3000;
 // Entorno
 // ==============================
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
 // ==============================
 // Base De Datos
 // ==============================
 let urlDataBase;
-//if (process.env.NODE_ENV === 'dev') {
-//    urlDataBase = 'mongodb://localhost:27018/cafe';
-//} else {
-urlDataBase = 'mongodb+srv://jhon:J6IadXUA2VLV1wSY@cluster0-twiza.mongodb.net/cafe';
-//}
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDataBase = 'mongodb://localhost:27018/cafe';
+} else {
+    urlDataBase = process.env.MONGO_URI;
+}
 
 process.env.URLDB = urlDataBase;
